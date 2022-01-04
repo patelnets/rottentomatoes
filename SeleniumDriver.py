9d77a47d0be9f3fb7a34d3f2a619fa9f91344db6
vatsal <vatsal.patel15@imperial.ac.uk>
2022-01-04 21:18:53 +0000

    use new driver location

--- SeleniumDriver.py
+++ SeleniumDriver.py
@@ -2,14 +2,15 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 
+
 def getDriver():
     options = Options()
     options.add_argument("--window-size=1920,1200")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
 
-    DRIVER_PATH = '/Users/vatsal/Documents/RottenTomatoes/chromedriver'
-    s=Service(DRIVER_PATH)
+    DRIVER_PATH = '/Users/vatsal/Documents/github-personal/rottentomatoes/chromedriver'
+    s = Service(DRIVER_PATH)
 
     driver = webdriver.Chrome(options=options, service=s)
     return driver
